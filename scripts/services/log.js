00954ac3dca0347e6a261fabf7f840620896c7a4
Jerry <jerry9916@qq.com>
2015-02-14 23:15:21 +0800

    Log basic

--- scripts/services/log.js
+++ scripts/services/log.js
@@ -2,7 +2,18 @@
 angular.module(app.name).service('$logService',
   function ($http)
   {
+    this.getModules = function() {
+      return $http.get(app.api + '/logs/modules');
+    };
 
-    
+    this.getLogs = function(params) {
+      return $http.get(app.api + '/logs', {
+        params: params
+      });
+    };
+
+    this.deleteLog = function(id) {
+      return $http.delete(app.api + '/logs/'+id);
+    };
   }
 );
